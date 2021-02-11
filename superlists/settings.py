a52ae8cdbfd2c7b3122d46cb1ff73a4fbbb14fae
nickypoofate <nicklaus.d.faith.esquire@gmail.com>
2021-02-11 13:37:43 -0600

    Update settings.py

--- superlists/settings.py
+++ superlists/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!arz3khp0lnirpap3yu6k9^)-lax3@kpz81pc*k9xpx+!i-vzy'
+SECRET_KEY = 'secret'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
