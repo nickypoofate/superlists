1c158bcf137ac492f776c66d27796c5edb7e5f82
nickypoofate <nicklaus.d.faith.esquire@gmail.com>
2021-02-12 16:12:56 -0600

    use env vars for prod settings Debug, Allowed Hosts, Secret Key

--- superlists/settings.py
+++ superlists/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 #DEBUG = True
 
 #ALLOWED_HOSTS = ['*']
-if 'DJANGO_DEBUG_FALSE in os.environ:
+if 'DJANGO_DEBUG_FALSE' in os.environ:
   DEBUG = False
   SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
   ALLOWED_HOSTS = [os.environ['SITENAME']]
